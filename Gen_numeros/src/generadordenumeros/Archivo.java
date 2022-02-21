e778de2065362a3047607f7c465ace12057654ee
DiegoRicardo26 <Diegodeleon12345@gmail.com>
2022-02-21 14:43:46 -0600

    colas y pilas 1

---
+++ Gen_numeros/src/generadordenumeros/Archivo.java
@@ -0,0 +1,98 @@
+
+package generadordenumeros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+
+public class Archivo {
+    public static void Generar() throws IOException {
+        HashSet<Integer> lista_numeros = new HashSet<>();
+        while(lista_numeros.size()<1000000)
+        {
+            int random = ThreadLocalRandom.current().nextInt(-10000000,10000000+1);
+            lista_numeros.add(random);
+        }
+        //System.out.println("Hay: "+lista_numeros.size()+" numeros en el array");
+        
+        String fichero = "numeros.txt";
+ 
+        try {
+            File archivo= new File(fichero);
+            FileWriter escribir= new FileWriter(archivo,true);
+            PrintWriter imprimir=new PrintWriter(escribir);   
+            imprimir.print(lista_numeros);
+            imprimir.close();
+            System.out.println("Numeros generados correctamente...");
+ 
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Error: El fichero no existe. ");
+        }
+ 
+    }
+    //////////////////////////////////Ordenar////////////////////////////
+    public static void Ordenar() throws IOException
+    {
+        
+        File archivo = new File("Ordenados.txt");
+        archivo.createNewFile();
+        
+        ArrayList<String> lineas = new ArrayList<>();
+        Scanner leer = new Scanner(new File("numeros.txt")).useDelimiter(",");
+        while(leer.hasNext())
+        {
+            lineas.add(leer.next());
+        }
+
+        ArrayList<Integer> resultado = Pasar_Arr_Int(lineas);
+        Collections.sort(resultado);
+        //ArrayList<Integer> ordenado = burbuja(resultado);
+        //List<Integer> listOfInteger = lineas.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+
+        FileWriter escribir = new FileWriter(archivo);
+        PrintWriter imprimir=new PrintWriter(escribir);   
+        imprimir.print(resultado);
+        imprimir.close();
+    }
+    
+    public static ArrayList<Integer> Pasar_Arr_Int(ArrayList<String>numeros){
+        ArrayList<Integer> Convertir = new ArrayList<Integer>();
+        for(String stringValue : numeros){
+            try{
+                Convertir.add(Integer.parseInt(stringValue.trim()));
+            }catch (Exception ex) {
+            }
+        }
+        return Convertir;
+    }
+    
+    
+    public static ArrayList<Integer> burbuja(ArrayList<Integer> arreglo)
+    {
+      int auxiliar;
+      ArrayList<Integer> arregloOrdenado = new ArrayList<>();
+      for(int i = 1; i < arreglo.size(); i++)
+      {
+        for(int j = 0;j < arreglo.size()-i;j++)
+        {
+          if(arreglo.get(j) > arreglo.get(j+1))
+          {
+            auxiliar = arreglo.get(j);
+            arreglo.set(j, arreglo.get(j+1));
+            arreglo.set(j+1,  auxiliar);
+          }   
+        }
+      }
+      arregloOrdenado = arreglo;
+      return arregloOrdenado;
+    }
+}
---
+++ Gen_numeros/src/generadordenumeros/Main.java
@@ -0,0 +1,26 @@
+
+package generadordenumeros;
+import Vistas.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+     
+        frm_Estructuras estructuras = new frm_Estructuras();
+        estructuras.show();
+        ;
+        
+        
+        
+    }
+    
+}
